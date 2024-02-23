28b3ca63d338437c2d5bb29e118bace0ebe479ea
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-02-23 10:04:18 +0530

    UseId

--- currencyConvertor/src/components/InputBox.jsx
+++ currencyConvertor/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useId} from 'react'
 
 
 function InputBox({
@@ -10,19 +10,19 @@ function InputBox({
     selectCurrency = 'usd',
     amountDisable = false,
     currencyDisable = false
-    
     className = "",
 }) {
+    const amountInputId = useId()
    
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex `}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -36,12 +36,22 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange &&
+                    onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
                 >
                     
-                        <option value="usd">
-                            usd
-                        </option>
+                       {currencyOption.map((currency) 
+                        =>(
+                             <option key={currency}value= 
+                             {currency}>
+                             {currency}
+                             </option>
+                        ) )}
+                    
+                    
+                       
                 
                 </select>
             </div>
