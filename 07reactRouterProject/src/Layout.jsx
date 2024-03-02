349810ca900143cfa99185f97daa14bafa662908
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-02 10:53:47 +0530

    file uploading from chai aur code

--- 07reactRouterProject/src/Layout.jsx
+++ 07reactRouterProject/src/Layout.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
     <>
     <Header/>
-    <Outlet/>
-    <Footer/>
+    <Outlet />
+    <Footer />
     </>
   )
 }
