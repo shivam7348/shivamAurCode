e0410203944e473687df88f569afce3313edeba1
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-03-02 10:42:54 +0530

    Creating browisng Router

--- 07reactRouterProject/src/main.jsx
+++ 07reactRouterProject/src/main.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import {RouterProvider} from 'react-router-dom'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Layout />,
+    children= [
+      {path: "",
+      element: <Home />
+    },
+    {
+      path: 'about',
+      element: <About />
+    }
+    
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
